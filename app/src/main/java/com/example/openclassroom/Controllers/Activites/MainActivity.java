9035d5e116855022f731ce844808f741d2571aee
jacobitkashala <jacobitkashala@gmail.com>
2022-04-28 17:38:22 -0400

    add:action button

--- app/src/main/java/com/example/openclassroom/Controllers/Activites/MainActivity.java
+++ app/src/main/java/com/example/openclassroom/Controllers/Activites/MainActivity.java
@@ -3,14 +3,23 @@ package com.example.openclassroom.Controllers.Activites;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
+import com.example.openclassroom.Controllers.Fragment.MainFragment;
 import com.example.openclassroom.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements MainFragment.onButtonClickedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onButtonCliked(View view) {
+        System.out.println("Clicked");
+        //Log.e("tag","Cliked bton");
     }
 }
--- app/src/main/java/com/example/openclassroom/Controllers/Fragment/MainFragment.java
+++ app/src/main/java/com/example/openclassroom/Controllers/Fragment/MainFragment.java
@@ -1,7 +1,9 @@
 package com.example.openclassroom.Controllers.Fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,19 +11,31 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.openclassroom.R;
+import com.google.android.material.button.MaterialButtonToggleGroup;
 
 /**
  * A simple {@link Fragment} subclass.
  * Use the {@link MainFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * create an instance of this fragment. implements View.OnClickListener {
  */
-public class MainFragment extends Fragment {
+
+public class MainFragment extends Fragment implements  View.OnClickListener{
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    // cette collback permet de lien le fragment a notre activité
+    private onButtonClickedListener mCallback;
+
+    /*On declare une interface dans le but d'obliger toute activité
+  qui veulle communiquer avec cette fragment d'implementer la methode  onButton ClickedListener
+   */
+    public  interface onButtonClickedListener{
+
+        public void onButtonCliked(View view);
+    }
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -48,6 +62,9 @@ public class MainFragment extends Fragment {
         return fragment;
     }
 
+
+
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,13 +73,38 @@ public class MainFragment extends Fragment {
             mParam2 = getArguments().getString(ARG_PARAM2);
         }
     }
-/*
-* Permet de créer la vue du fragment
-* */
+
+    /**
+    * Permet de créer la vue du fragment
+    */
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View result= inflater.inflate(R.layout.fragment_main, container, false);
+       /*
+       * On capte tout le click
+       * */
+        result.findViewById(R.id.fragment_main_button).setOnClickListener(this);
+        return result;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        this.createCallbackToParentActivity();
+    }
+
+    @Override
+    public void onClick(View view) {
+        mCallback.onButtonCliked(view);
+    }
+    private void createCallbackToParentActivity(){
+        try {
+            //Parent activity will automatically subscribe to callback
+            mCallback = (onButtonClickedListener) getActivity();
+        } catch (ClassCastException e) {
+            throw new ClassCastException(e.toString()+ " must implement OnButtonClickedListener");
+        }
     }
 }
