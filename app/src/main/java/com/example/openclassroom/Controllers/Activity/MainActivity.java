d666bc65910e93560f7c316619f1705a3b63b7e0
jacobitkashala <jacobitkashala@gmail.com>
2022-04-29 12:36:53 -0400

    implement MVC

--- app/src/main/java/com/example/openclassroom/Controllers/Activity/MainActivity.java
+++ app/src/main/java/com/example/openclassroom/Controllers/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.openclassroom;
+package com.example.openclassroom.Controllers.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.openclassroom.R;
 
 public class MainActivity extends AppCompatActivity {
 
